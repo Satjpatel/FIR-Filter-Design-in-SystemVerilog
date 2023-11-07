03410f1a7bca92b573c46b9c6b1a8e2401cd314d
Sat J. Patel <satjpatel007@gmail.com>
2023-11-07 00:43:13 -0600

    Update test_fir_filter.py

--- sim/test_fir_filter.py
+++ sim/test_fir_filter.py
@@ -1,7 +1,7 @@
 from cocotb_test.simulator import run
 def test_fir_filter():
     run(
-        verilog_sources=["fir_filter.sv", "dflopie.sv"], # sources
+        verilog_sources=["../srcs/fir_filter.sv", "../srcs/dflopie.sv"], # sources
         toplevel="fir_filter",            # top level HDL
         module="tb_fir_filter"        # name of cocotb test module
     )
